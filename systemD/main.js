50c0b3e8b750e6ac0eaa0b417ec0a89cf74e7c86
Damien Gurzynski <30811486+damiengurzynski@users.noreply.github.com>
2025-04-14 00:44:57 +0200

    Update main.js

--- systemD/main.js
+++ systemD/main.js
@@ -172,7 +172,7 @@ function modifyBody()
 
 function help()
 {
-  alert("A questionably accurate star system simulator.\n\n/ Shortkeys /\n\n<Space> : Start/Stop simulation\n\n<Arrow Keys> : Move the map around\n\n<,> : Zoom in\n\n<;> : Zoom out\n\n/ Interface /\n\nTIME (slider) : simulation speed\n\nGRAVITY (slider) : gravitational constant (strength)\n\nBODY : Fill the parameters then click [New Body] to create a new body on screen\n\n[Modify] : Click a body on screen (it will highlight when selected), it's parameters will show in the left boxes, you can modify any of them and click [Modify] to apply the changes\n\n[New System] : Create a blank system (! erases current system)\n\n[Save System] : Generates a 'save code' in the box below, copy and paste it somewhere for safekeeping\n\n[Load System] : Paste a 'save code' in the input box and press this to load it");
+  alert("<Space> : Start/Stop simulation\n\n<Arrow Keys> : Move the map around\n\n<,> : Zoom in\n\n<;> : Zoom out\n\nTIME (slider) : simulation speed\n\nGRAVITY (slider) : gravitational constant (strength)\n\nBODY : Fill the parameters then click [New Body] to create a new body on screen\n\n[Modify] : Click a body on screen (it will highlight when selected), it's parameters will show in the left boxes, you can modify any of them and click [Modify] to apply the changes\n\n[New System] : Create a blank system (! erases current system)\n\n[Save System] : Generates a 'save code' in the box below, copy and paste it somewhere for safekeeping\n\n[Load System] : Paste a 'save code' in the input box and press this to load it");
 }
 
 
